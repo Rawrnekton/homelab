f685d6e070fac5151fac168e0148299e04430997
Jonathan Gerdes <jona@cindergla.de>
2024-09-05 14:57:01 +0200

    initial commit

---
+++ ansible/roles/certbot/files/new_cert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Überprüfen, ob ein Service-Name übergeben wurde
+if [ -z "$1" ]; then
+  echo "Bitte den Namen des Services als Parameter übergeben."
+  exit 1
+fi
+
+# Der Service-Name wird als erster Parameter übergeben
+service_name=$1
+
+# Docker-Befehl ausführen
+docker run --rm \
+  -v /root/.secrets/certbot/netcup.ini:/root/.secrets/certbot/netcup.ini \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  --cap-drop=all \
+  certbot-dns-netcup:latest certbot certonly \
+  --authenticator dns-netcup \
+  --dns-netcup-propagation-seconds 1200 \
+  --dns-netcup-credentials /root/.secrets/certbot/netcup.ini \
+  --keep-until-expiring --non-interactive --expand \
+  --server https://acme-v02.api.letsencrypt.org/directory \
+  --agree-tos --email "jonathangerdes1@aol.de" \
+  -d "$service_name.cindergla.de" -d "$service_name.k8s.internal.cindergla.de"
